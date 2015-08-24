615552557c9ce39c21917c7bfda4cfbe811f707e
blahaf@gmail.com <blahaf@gmail.com>
2015-08-24 22:41:30 +0200

    code improvement

--- minsk/tests/eval/test_manager.py
+++ minsk/tests/eval/test_manager.py
@@ -1,6 +1,6 @@
 import testtools
 
-import minsk.eval.manager
+import minsk.eval.manager as manager
 import minsk.tests.eval.common as common
 import minsk.model as model
 
@@ -8,7 +8,7 @@ import minsk.model as model
 class TestEvaluatorManager(testtools.TestCase, common.TestUtilsMixin):
     def setUp(self):
         super().setUp()
-        self.manager = minsk.eval.manager.EvaluatorManager()
+        self.manager = manager.EvaluatorManager()
 
     def test_find_four_of_kind(self):
         self._test_hand('Js Jc 2h Jh 2c 3c Jd', model.Hand.FOUR_OF_KIND)
