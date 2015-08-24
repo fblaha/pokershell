60e264a8f24cb0c762e255d8cc866a6cf1901696
blahaf@gmail.com <blahaf@gmail.com>
2015-08-24 18:01:03 +0200

    simulation generalized to pre river phases

--- minsk/eval/simulation.py
+++ minsk/eval/simulation.py
@@ -10,6 +10,21 @@ class BruteForceSimulator:
         self._manager = manager.EvaluatorManager()
 
     def simulate(self, *cards):
+        unknown_count = 7 - len(cards)
+        deck = model.Deck(*cards)
+        deck_cards = deck.cards
+        win, tie, lose = 0, 0, 0
+        if unknown_count:
+            for unknown in itertools.combinations(deck_cards, unknown_count):
+                result = self.simulate_river(*(cards + unknown))
+                win += result[0]
+                tie += result[1]
+                lose += result[2]
+        else:
+            return self.simulate_river(*cards)
+        return win, tie, lose
+
+    def simulate_river(self, *cards):
         common = cards[2:]
         deck = model.Deck(*cards)
         deck_cards = deck.cards
@@ -19,7 +34,7 @@ class BruteForceSimulator:
             opponent_cards = opponent + common
             opponent_best = self._manager.find_best_hand(*opponent_cards)
             if len(best_hand) != len(opponent_best):
-                raise ValueError('Hands are not comarable: {0} {1}'.
+                raise ValueError('Hands are not comparable: {0} {1}'.
                                  format(best_hand, opponent_best))
             if best_hand > opponent_best:
                 win += 1
--- minsk/tests/eval/test_simulation.py
+++ minsk/tests/eval/test_simulation.py
@@ -9,6 +9,6 @@ class TestBruteForceSimulator(testtools.TestCase, common.TestUtilsMixin):
         super().setUp()
         self.simulator = simulation.BruteForceSimulator()
 
-    def test_brute_force(self):
-        cards = self.parse_cards('6s 8c 2h 8h 2c 3c Jd')
+    def test_brute_force_turn(self):
+        cards = self.parse_cards('6s 8c 2h 8h 2c 3c')
         print(self.simulator.simulate(*cards))
