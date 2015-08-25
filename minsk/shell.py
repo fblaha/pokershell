467d81e2b34e3987a255b6de52b394ec7a5bc90c
blahaf@gmail.com <blahaf@gmail.com>
2015-08-25 19:33:54 +0200

    code improvement

--- minsk/shell.py
+++ minsk/shell.py
@@ -15,34 +15,35 @@ class MinskShell(cmd.Cmd):
         simulator = simulation.BruteForceSimulator()
         cards = model.Card.parse_cards(cards)
 
+        self.print_input(cards)
+        result = simulator.simulate(*cards)
+        self.print_output(result)
+
+    def print_output(self, result):
+        print('\nOutput :')
+        total = sum(result)
+        result_pct = list(map(lambda x: str(round(x / total * 100)) + '%', result))
+        result_table = prettytable.PrettyTable(["Win", "Tie", "Loss"])
+        result_table.add_row(result_pct)
+        result_table.add_row(result)
+        print(result_table)
+
+    def print_input(self, cards):
         print('\nInput :')
         columns = ['Hole']
         row = [' '.join(map(repr, cards[0:2]))]
         if len(cards) >= 5:
             columns.append('Flop')
             row.append(' '.join(map(repr, cards[2:5])))
-
         if len(cards) >= 6:
             columns.append('Turn')
             row.append(cards[5])
         if len(cards) == 7:
             columns.append('River')
             row.append(cards[6])
-
         input_table = prettytable.PrettyTable(columns)
         input_table.add_row(row)
         print(input_table)
-
-        print('\nOutput :')
-        result = simulator.simulate(*cards)
-        total = sum(result)
-        result_pct = list(map(lambda x: str(round(x / total * 100)) + '%', result))
-
-        result_table = prettytable.PrettyTable(["Win", "Tie", "Loss"])
-
-        result_table.add_row(result_pct)
-        result_table.add_row(result)
-        print(result_table)
 
     def do_EOF(self, line):
         return True
