5bb7d1b08ba30d800a284f89c2b301dd562d18f4
blahaf@gmail.com <blahaf@gmail.com>
2015-08-25 09:16:31 +0200

    simulation tests

--- minsk/tests/eval/test_simulation.py
+++ minsk/tests/eval/test_simulation.py
@@ -12,3 +12,15 @@ class TestBruteForceSimulator(testtools.TestCase, common.TestUtilsMixin):
     def test_brute_force_turn(self):
         cards = self.parse_cards('6s 8c 2h 8h 2c 3c')
         print(self.simulator.simulate(*cards))
+
+    def test_full_house(self):
+        cards = self.parse_cards('As 6c Ad 8s Ac 6d')
+        result = self.simulator.simulate(*cards)
+        print(result)
+        self.assertTrue(result[0] / sum(result) > 0.9)
+
+    def test_bad_luck(self):
+        cards = self.parse_cards('2c 4d 8c Js Qd Qc')
+        result = self.simulator.simulate(*cards)
+        print(result)
+        self.assertTrue(result[0] / sum(result) < 0.2)
