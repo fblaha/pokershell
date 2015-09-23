244ead01b7463b6c8780fda622fda6360928b3bf
blahaf@gmail.com <blahaf@gmail.com>
2015-09-23 23:04:22 +0200

    pycharm suggestions

--- minsk/tests/eval/common.py
+++ minsk/tests/eval/common.py
@@ -7,13 +7,14 @@ import minsk.eval.context as context
 
 @contextlib.contextmanager
 def test_config(sim_cycles, num_player):
-    orig_vals = config.sim_cycles, config.player_num
+    orig_values = config.sim_cycles, config.player_num
     config.sim_cycles, config.player_num = sim_cycles, num_player
     yield
-    config.sim_cycles, config.player_num = orig_vals[0], orig_vals[1]
+    config.sim_cycles, config.player_num = orig_values[0], orig_values[1]
 
 
 class TestUtilsMixin:
-    def create_context(self, cards_str):
+    @staticmethod
+    def create_context(cards_str):
         cards = model.Card.parse_cards(cards_str)
         return context.EvalContext(*cards)
