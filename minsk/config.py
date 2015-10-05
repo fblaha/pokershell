8364ca05548ee4f5fbb30f5dd68d37ed3b9c27fe
blahaf@gmail.com <blahaf@gmail.com>
2015-10-06 00:21:53 +0200

    fixing config

--- minsk/config.py
+++ minsk/config.py
@@ -1,12 +1,16 @@
 import contextlib
-
 player_num = 2
 sim_cycles = 40000
 
 
 @contextlib.contextmanager
-def with_config(_sim_cycles=sim_cycles, _player_num=player_num):
+def with_config(_sim_cycles=None, _player_num=None):
     global sim_cycles, player_num
+    if not _sim_cycles:
+        _sim_cycles = sim_cycles
+    if not _player_num:
+        _player_num = player_num
+    print(sim_cycles)
     orig_values = sim_cycles, player_num
     sim_cycles, player_num = _sim_cycles, _player_num
     yield
--- minsk/tests/test_config.py
+++ minsk/tests/test_config.py
@@ -11,6 +11,15 @@ class TestConfig(testtools.TestCase):
             self._check_values(20, 9999)
         self._check_values(orig_player_num, orig_sim_cycles)
 
+    def test_with_config_inner(self):
+        orig_sim_cycles = config.sim_cycles
+        orig_player_num = config.player_num
+        with config.with_config(9999, 20):
+            self._check_values(20, 9999)
+            with config.with_config(_player_num=21):
+                self._check_values(21, 9999)
+        self._check_values(orig_player_num, orig_sim_cycles)
+
     def _check_values(self, expected_player_num, expected_sim_cycles):
         self.assertEqual(expected_sim_cycles, config.sim_cycles)
         self.assertEqual(expected_player_num, config.player_num)
