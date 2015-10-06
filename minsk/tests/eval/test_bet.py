aefb14c0fdaa45150ee3dccd924f4e7e468cc0f1
blahaf@gmail.com <blahaf@gmail.com>
2015-10-06 22:18:51 +0200

    flop max bet

--- minsk/tests/eval/test_bet.py
+++ minsk/tests/eval/test_bet.py
@@ -16,3 +16,14 @@ class TestBetAdviser(testtools.TestCase):
                         self.adviser.get_max_bet(0.4, 100) <
                         self.adviser.get_max_bet(0.5, 100) <
                         self.adviser.get_max_bet(0.6, 100))
+
+    def test_max_bet_flop(self):
+        self._test_max_bet_flop(0.1)
+        self._test_max_bet_flop(0.3)
+        self._test_max_bet_flop(0.5)
+        self._test_max_bet_flop(0.7)
+        self._test_max_bet_flop(0.9)
+
+    def _test_max_bet_flop(self, strength):
+        self.assertTrue(self.adviser.get_max_bet(strength, 100) >
+                        self.adviser.get_max_bet(strength, 100, 5))
