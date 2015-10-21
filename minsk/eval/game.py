37f7ca9d67dc45f1c58fd2dd92ab263caf4c997c
blahaf@gmail.com <blahaf@gmail.com>
2015-10-21 22:48:40 +0200

    game stack

--- minsk/eval/game.py
+++ minsk/eval/game.py
@@ -8,6 +8,21 @@ class GameState:
     def is_successor(self, other):
         my_cards = ''.join(map(repr, self._cards))
         other_cards = ''.join(map(repr, other._cards))
-        return my_cards.startswith(other_cards) \
-               and self._pot >= other._pot \
-               and self._player_num <= other._player_num
+        return my_cards.startswith(other_cards) and \
+               self._pot >= other._pot and \
+               self._player_num <= other._player_num
+
+
+class GameStack:
+    def __init__(self):
+        super().__init__()
+        self._stack = []
+
+    def add_state(self, state):
+        if self._stack and not state.is_successor(self._stack[-1]):
+            self._stack = []
+        self._stack.append(state)
+
+    @property
+    def stack(self):
+        return self._stack
--- minsk/tests/eval/test_game.py
+++ minsk/tests/eval/test_game.py
@@ -3,22 +3,37 @@ import testtools
 import minsk.eval.game as game
 import minsk.model as model
 
+parse = model.Card.parse_cards_line
+
+state = game.GameState
+
 
 class TestGameState(testtools.TestCase):
     def test_successor_cards(self):
-        pre_flop = game.GameState(model.Card.parse_cards_line("2d 2c"), 5, 100)
-        flop = game.GameState(model.Card.parse_cards_line("2d 2c 4h 5d 8h"), 2, 200)
+        pre_flop = state(parse("2d 2c"), 5, 100)
+        flop = state(parse("2d 2c 4h 5d 8h"), 2, 200)
         self.assertTrue(flop.is_successor(pre_flop))
         self.assertFalse(pre_flop.is_successor(flop))
 
     def test_successor_player_num(self):
-        pre_flop = game.GameState(model.Card.parse_cards_line("2d 2c"), 5, 100)
-        flop = game.GameState(model.Card.parse_cards_line("2d 2c"), 2, 200)
-        self.assertTrue(flop.is_successor(pre_flop))
-        self.assertFalse(pre_flop.is_successor(flop))
+        pre_flop1 = state(parse("2d 2c"), 5, 100)
+        pre_flop2 = state(parse("2d 2c"), 2, 200)
+        self.assertTrue(pre_flop2.is_successor(pre_flop1))
+        self.assertFalse(pre_flop1.is_successor(pre_flop2))
 
     def test_successor_pot(self):
-        pre_flop = game.GameState(model.Card.parse_cards_line("2d 2c"), 5, 100)
-        flop = game.GameState(model.Card.parse_cards_line("2d 2c"), 5, 200)
-        self.assertTrue(flop.is_successor(pre_flop))
-        self.assertFalse(pre_flop.is_successor(flop))
+        pre_flop1 = state(parse("2d 2c"), 5, 100)
+        pre_flop2 = state(parse("2d 2c"), 5, 200)
+        self.assertTrue(pre_flop2.is_successor(pre_flop1))
+        self.assertFalse(pre_flop1.is_successor(pre_flop2))
+
+
+class TestGameStack(testtools.TestCase):
+    def test_stack(self):
+        stack = game.GameStack()
+        stack.add_state(state(parse("2d 2c"), 5, 100))
+        self.assertEqual(1, len(stack.stack))
+        stack.add_state(state(parse("2d 2c 4h 5d 8h"), 2, 200))
+        self.assertEqual(2, len(stack.stack))
+        stack.add_state(state(parse("3d 4c"), 5, 100))
+        self.assertEqual(1, len(stack.stack))
