975f2b7e18450b93aeeb3b738960bd3b30be800a
blahaf@gmail.com <blahaf@gmail.com>
2015-11-07 02:02:29 +0100

    fixing input table player_num column

--- minsk/shell.py
+++ minsk/shell.py
@@ -230,8 +230,8 @@ class MinskShell(cmd.Cmd):
             if len(cards) == 7:
                 table[InputTableColumn.RIVER].append(cards[6])
 
-            player_num = state.player_num or config.player_num
-            table[InputTableColumn.PLAYER_NUM].append(player_num)
+            if state.player_num:
+                table[InputTableColumn.PLAYER_NUM].append(state.player_num)
 
             if len(cards) >= 5:
                 evaluator_manager = manager.EvaluatorManager()
