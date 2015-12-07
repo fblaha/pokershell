8c658ea1f737b8d0b7d85c78d4358d2a8b5efd26
blahaf@gmail.com <blahaf@gmail.com>
2015-12-07 22:33:02 +0100

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,40 +1,27 @@
 #!/usr/bin/env python
 
-PROJECT = 'pokershell'
+import setuptools
 
-# Change docs/sphinx/conf.py too!
+PROJECT = 'pokershell'
 VERSION = '0.1'
 
-from setuptools import setup, find_packages
-
-setup(
+setuptools.setup(
     name=PROJECT,
     version=VERSION,
-
     description='Poker Shell',
-
     author='Filip Blaha',
     author_email='blahaf@gmail.com',
-
     classifiers=['Environment :: Console', ],
-
     platforms=['Any'],
-
-    scripts=[],
-
-    provides=[],
-
     namespace_packages=[],
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     package_data={
         'pokershell.eval': ['preflop/*.txt']},
     include_package_data=True,
-
     entry_points={
         'console_scripts': [
             'pokershell = pokershell.shell:main'
         ]
     },
-
     zip_safe=False,
 )
